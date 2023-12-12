9b106a1b0088bc8dae6598d455545b785c69c953
saniarizqi <sania.rizqi@ui.ac.id>
2023-12-12 21:27:42 +0700

    authentication, reservasi

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
+from utils.query import query
 
 # Create your views here.
 def show_customer(request):
-    return render(request, "dash_cust.html")
+    data = query(f"""
+                SELECT U.fname, U.lname, RA.email, RA.phonenum, C.nik
+                FROM user_table U
+                JOIN RESERVATION_ACTOR RA ON RA.email = U.email 
+                LEFT JOIN CUSTOMER C ON C.email = RA.email;
+                """)
+    # print(data)
+    context = {
+        "data_cust" : data
+    }
+    return render(request, "dash_cust.html", context)
 
 def show_hotel(request):
-    return render(request, "dash_hotel.html")
+    data = query(f"""
+                 SELECT 
+                 h.street, h.district, h.city, h.province,
+                 h.hotel_name, h.hotel_branch, h.nib, h.email,
+                 ra.phonenum,
+                 u.fname, u.lname
+                 FROM hotel h
+                 RIGHT JOIN reservation_actor ra ON h.email = ra.email
+                 RIGHT JOIN user_table u ON h.email = u.email
+                 WHERE ra.email LIKE '%.hotel%';
+                 """)
+
+    context= {
+        "data_hotel" : data
+    }
+    return render(request, "dash_hotel.html", context)
